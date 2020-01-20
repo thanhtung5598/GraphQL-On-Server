c428f064d78afc2eab41b829b9d0c3b7832de5ac
LeThanhTung <thanhtung5598@gmail.com>
2020-01-20 16:08:08 +0700

    Model and mongoose

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const GrapqlHTTP = require('express-graphql');
-const schema = require('./schema/schema')
+const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();
+// connected to mlab database
+mongoose.connect('mongodb://ninja:test@ds161148.mlab.com:61148/graphql-ninja')
+mongoose.connection.once('open', () => {
+    console.log('conneted to database');
+});
 
-app.use('/graphql',GrapqlHTTP({
+app.use('/graphql', GrapqlHTTP({
     schema,
-    graphiql:true
+    graphiql: true
 }));
 
-app.listen(4000,()=>{
+app.listen(4000, () => {
     console.log("listening on port 4000");
 })
